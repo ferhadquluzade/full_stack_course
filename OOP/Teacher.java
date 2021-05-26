c002b95d6d4fa151d91742b50d661bcfba487243
ferhadquluzade <62217940+ferhadquluzade@users.noreply.github.com>
2021-05-26 21:04:38 +0400

    Add files via upload

--- OOP/Teacher.java
+++ OOP/Teacher.java
@@ -1,22 +1,54 @@
-package com.company;
-
-public class Teacher {
-    String name;
-    int id;
-    public Teacher(String name,int id){
+import java.util.*;
+import java.lang.*;
+class Teacher{
+private String name;
+private int age;
+private int salary;
+public Teacher(String name,int age,int salary){
 this.name=name;
-this.id=id;
-    }
-    public String getName(){
-        return this.name;
-    }
-    public void setName(String name){
-        this.name=name;
-    }
-    public int getId(){
-        return this.id;
-    }
-    public void setId(int id){
-        this.id=id;
-    }
+this.age=age;
+this.salary=salary;
 }
+public String getName(){
+return name;
+}
+public int getAge(){
+return age;}
+public int getSalary(){
+return salary;
+}
+public void setName(String newName){
+name=newName;
+}
+;public void setAge(int newAge){
+age=newAge;
+}
+public void setSalary(int newSalary){
+salary=newSalary;
+}
+public String toString(){
+return String.format("name: %s,salary :%d,age: %d",name,salary,age);
+}
+public  void addSalary(int wageToAdd){
+salary+=wageToAdd;
+}
+public static void main(String[] args){
+Teacher[] tcArr={new Teacher("Dadas",48,1000),
+
+	         new Teacher("Lala",24,1500),
+	         new Teacher("Fikret",25,1025)};
+for(Teacher tch:tcArr)
+System.out.println(tch.toString());
+Arrays.sort(tcArr,(a,b) -> Integer.compare(a.salary,b.salary));
+System.out.println(tcArr[2].toString());
+for(Teacher tch:tcArr){
+if(tch.age>45)
+tch.salary+=100;
+}
+for(Teacher tch:tcArr)
+System.out.println(tch.salary);
+
+
+}
+}
+
